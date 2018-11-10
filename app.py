d4fdc1257bcad4eae2bbef640c49bc8bb6723c5d
Harshad Reddy Nalla <harshadreddy16@gmail.com>
2018-11-10 17:01:26 -0500

    added the flask application

---
+++ app.py
@@ -0,0 +1,7 @@
+from passlib.hash import pbkdf2_sha256
+from flask import Flask, render_template, request, session, flash, url_for, redirect, make_response
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', threaded=True)
