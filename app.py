9ca622e9446afa0cc0d12587db436ef0f2fe9817
Harshad Reddy Nalla <harshadreddy16@gmail.com>
2018-11-10 22:16:56 -0500

    fixing errors

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from passlib.hash import pbkdf2_sha256
 from flask import Flask, render_template, request, session, flash, url_for, redirect, make_response
 
 # Use a service account
-cred = credentials.Certificate('victory-222122-8264af68af1c.json')
+cred = credentials.Certificate('config.json')
 firebase_admin.initialize_app(cred)
 
 # database connection
@@ -14,7 +14,7 @@ db = firestore.client()
 
 # flask connection
 app = Flask(__name__)
-app.secret_key = json.load(open('victory-222122-8264af68af1c.json', 'r')).get('project_id', 'qwerty')
+app.secret_key = json.load(open('config.json', 'r')).get('project_id', 'qwerty')
 app.config['SESSION_TYPE'] = 'filesystem'
 
 
@@ -22,6 +22,11 @@ app.config['SESSION_TYPE'] = 'filesystem'
 @app.route('/home', methods=['GET'])
 def home():
     return render_template('signup.html')
+
+
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    return render_template('dashboard.html')
 
 
 @app.route('/userLogin', methods=['GET', 'POST'])
@@ -39,10 +44,10 @@ def user_login():
             return render_template('login.html', error=err)
         else:
             session['username'] = user.get('email')
-            session['doctor'] = True
+            session['acc_type'] = user.get('acc_type')
             session['logged_in'] = True
             msg = 'You have been successfully logged'
-            return redirect(url_for('user_login', msg=msg))
+            return redirect(url_for('dashboard', msg=msg))
     return render_template('login.html')
 
 
@@ -51,17 +56,19 @@ def user_signup():
     if request.method == 'POST':
         print(request.form.get('email'))
         user_details = {
-            'full_name' : request.form.get('full_name'),
+            'full_name': request.form.get('full_name'),
             'email': request.form.get('email'),
             'password': pbkdf2_sha256.hash(str(request.form.get('password'))),
-            'dob':'{}-{}-{}'.format(request.form.get("dob_mm"),request.form.get("dob_mm"),request.form.get("dob_yy")),
-            'gender':request.form.get("gender"),
+            'dob': '{}-{}-{}'.format(request.form.get("dob_mm"),
+                                     request.form.get("dob_mm"),
+                                     request.form.get("dob_yy")),
+            'gender': request.form.get("gender"),
             'acc_type': request.form.get('acc_type'),
-            'about':request.form.get('Highlight'),
-            'pay_mode':request.form.get('payment-method'),
-            'card_number':pbkdf2_sha256.hash(str(request.form.get('card-number'))),
-            'pin':pbkdf2_sha256.hash(str('pin'))
-            
+            'about': request.form.get('Highlight'),
+            'pay_mode': request.form.get('payment-method'),
+            'card_number': pbkdf2_sha256.hash(str(request.form.get('card-number'))),
+            'pin': pbkdf2_sha256.hash(str('pin'))
+
         }
 
         user = db.collection('users').document(str(request.form.get('email')))
@@ -72,13 +79,12 @@ def user_signup():
         msg = 'You have been successfully logged'
         return redirect(url_for('dashboard', msg=msg))
 
-    return render_template('signup.html',message="Unsuccessful! Try again")
-
+    return render_template('signup.html', message="Unsuccessful! Try again")
 
 
 @app.route('/jobPosting', methods=['GET', 'POST'])
 def job_posting():
-    pass
+    return
 
 
 @app.route('/logout')
@@ -91,12 +97,12 @@ def logout():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    pass
+    return
 
 
 @app.route('/testing')
 def testing():
-    pass
+    return
 
 
 if __name__ == "__main__":
